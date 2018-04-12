635a590b20a1fa83513f38781a5d529327e7b7cc
Michael Pascuzzi <gatorpazz@gmail.com>
2018-04-12 10:53:30 -0400

    Scaffolding out players and teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
 
 export default class Teams extends Component {
-    render() {
-        return (
-            <div className='container two-column'>
-                Teams
-            </div>
-        )
-    }
+  state = {
+    teamNames: [],
+    loading: true
+  }
+  componentDidMount() {
+    getTeamNames()
+      .then(teamNames => {
+        this.setState({loading: false, teamNames})
+      })
+  }
+  render() {
+    const { loading, teamNames } = this.state
+    const { location, match } = this.props
+
+    return (
+      <div className='container two-column'>
+        <Sidebar
+          loading={loading}
+          title='Teams'
+          list={teamNames}
+          {...this.props}
+        />
+
+        {loading === false && location.pathname === '/teams'
+          ? <div className='sidebar-instruction'>Select a Team</div>
+          : null
+        }
+      </div>
+    )
+  }
 }
