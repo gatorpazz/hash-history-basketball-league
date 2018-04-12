9907e43294f80f49469473918d7b1217681a9395
Michael Pascuzzi <gatorpazz@gmail.com>
2018-04-12 18:31:37 -0400

    Scaffolded out the team page

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import Sidebar from './Sidebar'
+import { Route, Link } from 'react-router-dom'
+
 import { getTeamNames } from '../api'
+import TeamLogo from './TeamLogo'
+import Team from './Team'
 
 export default class Teams extends Component {
   state = {
@@ -30,6 +34,31 @@ export default class Teams extends Component {
           ? <div className='sidebar-instruction'>Select a Team</div>
           : null
         }
+
+        <Route path={`${match.url}/:teamId`} render={({ match }) => (
+            <div className='panel'>
+              <Team id={match.params.teamId}>
+                  {team => team === null
+                    ? <h1>Loading</h1>
+                    : <div style={{width: '100%'}}>
+                        <TeamLogo id={team.id} className='center' />
+                        <h1 className='medium-header'>{team.name}</h1>
+                        <ul className='info-list row'>
+                          <li>Established<div>{team.established}</div></li>
+                          <li>Manager<div>{team.manager}</div></li>
+                          <li>Coach<div>{team.coach}</div></li>
+                        </ul>
+                        <Link
+                          className='center btn-main'
+                          to={`/${match.params.teamId}`}
+                        >
+                          {team.name} Team Page
+                        </Link>
+                      </div>
+                  }
+              </Team>
+            </div>
+        )} />
       </div>
     )
   }
